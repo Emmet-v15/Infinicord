8adc314610530acb70d34441253a4dba269f1c2f
Emmet <emmet@v15.studio>
2026-03-23 03:06:02 +0000

    fix: use icon.png as default splash image

--- src/main/userAssets.ts
+++ src/main/userAssets.ts
@@ -29,7 +29,7 @@ const CUSTOMIZABLE_ASSETS = [
 export type UserAssetType = (typeof CUSTOMIZABLE_ASSETS)[number];
 
 const DEFAULT_ASSETS: Record<UserAssetType, string> = {
-    splash: "tray.png",
+    splash: "icon.png",
     tray:
         process.platform === "darwin"
             ? "tray/trayTemplate.png"
